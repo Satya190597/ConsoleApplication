f45c6f917a42d148f1f87739aca3fcdae183e114
satya190597 <webprakashsatya@gmail.com>
2019-01-24 14:52:03 +0530

    Added User Input Function In My Code

--- satya_c#_assignment_one_v1/Program.cs
+++ satya_c#_assignment_one_v1/Program.cs
@@ -7,6 +7,29 @@ namespace satya_c__assignment_one_v1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int[] ar = Program.GetNumbers();
+            Console.WriteLine("This Is Great {0}",ar);
+        }
+        private static int[] GetNumbers()
+        {
+            int[] arr = new int[10];
+            int index = 0;
+            while(index<arr.Length)
+            {
+                Console.Write("Enter The {0} Number \t: ",(index+1));
+                string userInput = Console.ReadLine();
+                int elements;
+                if(Int32.TryParse(userInput,out elements))
+                {
+                    arr[index++] = elements;
+                }
+                else
+                {
+                    Console.WriteLine("Warning : Please Enter Numeric Value !");
+                }
+            }
+            Console.WriteLine(arr.Length);
+            return arr;
         }
     }
 }
